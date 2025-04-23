3c6082c71a78256a317a2d6677b6f555597c74ec
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-04-23 23:12:51 +0530

    updated the scripts for controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,5 +3,5 @@ import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 }
